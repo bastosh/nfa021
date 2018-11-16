dd314728bfadc9790399feebf8beee203d182802
Sébastien Pereda <sebastien.pereda@gmail.com>
2018-11-16 13:38:28 +0100

    Changed: Modification homepage pour test validation et déploiement

--- app/views/pages/home.view.php
+++ app/views/pages/home.view.php
@@ -1,5 +1,6 @@
 <?php require __DIR__.'/../partials/head.php'; ?>
   <header>
     <h1>Homepage</h1>
+    <p>Let’s start a project!</p>
   </header>
 <?php require __DIR__.'/../partials/footer.php'; ?>
