075455e67611cac0415bfaeacd98a11da69749e3
Sébastien Pereda <sebastien.pereda@gmail.com>
2018-11-26 08:47:04 +0100

    Added: Validation côté client pour l’update

--- app/views/features/edit.view.php
+++ app/views/features/edit.view.php
@@ -10,14 +10,27 @@
 
     <div class="grid-x margin-top-2">
       <div class="cell small-8 small-offset-2 medium-6 medium-offset-3">
-        <form action="/features/<?= $feature->id; ?>" method="POST">
+        <form action="/features/<?= $feature->id; ?>" method="POST" data-abide novalidate>
+
+          <div data-abide-error class="callout alert-callout-border alert" style="display: none;">
+            <p><i class="fi-alert"></i> There are some errors in your form.</p>
+          </div>
+
           <input type="hidden" name="_method" value="PUT">
           <label>Title
-            <input name="title" type="text" placeholder="Title of the feature" value="<?= $feature->title; ?>">
+            <input name="title" type="text" placeholder="Title of the feature" value="<?= $feature->title; ?>" required pattern="^.{3,50}$">
+            <span class="form-error">
+              Title is required and must contain between 3 and 50 characters.
+            </span>
           </label>
+          <p class="help-text">Required. Between 3 and 50 characters.</p>
           <label>Description
-            <textarea name="description" placeholder="Description of the feature" rows="3" ><?= $feature->description; ?></textarea>
+            <textarea name="description" placeholder="Description of the feature" rows="3" required pattern="^.{3,255}$"><?= $feature->description; ?></textarea>
+            <span class="form-error">
+              Description is required and must contain between 3 and 255 characters.
+            </span>
           </label>
+          <p class="help-text">Required. Between 3 and 255 characters.</p>
           <input type="submit" class="button" value="Submit">
         </form>
       </div>
