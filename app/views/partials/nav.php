9361300ccf3570acbd7dc12de2f009e6469b991d
Sébastien Pereda <sebastien.pereda@gmail.com>
2018-11-26 15:01:10 +0100

    Changed: Changement de libellés dans la barre de navigation

--- app/views/partials/nav.php
+++ app/views/partials/nav.php
@@ -5,8 +5,11 @@
   <li><a href="/contact">Contact</a></li>
 </ul>
 <ul class="menu padding-vertical-1">
-    <li><a href="/admin">Admin</a></li>
+    <?php if (!isset($_SESSION['username'])): ?>
+      <li><a href="/admin">Login</a></li>
+    <?php endif; ?>
     <?php if (isset($_SESSION['username'])): ?>
+      <li><a href="/admin">Dashboard</a></li>
       <li><a href="/logout">Logout</a></li>
     <?php endif; ?>
 </ul>
