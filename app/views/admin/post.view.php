32108998a90c65a463a5b3e36450ed737490176c
Sébastien Pereda <sebastien.pereda@gmail.com>
2018-11-28 11:20:58 +0100

    Changed: Mise à jour aperçu des articles dans l’administration

--- app/views/admin/post.view.php
+++ app/views/admin/post.view.php
@@ -4,19 +4,33 @@
   <div class="app-dashboard-body-content off-canvas-content" data-off-canvas-content>
 
     <a href="/admin-posts"><i class="fas fa-long-arrow-alt-left"></i> Back</a>
-
-    <h2 class="text-center"><?= $post->title; ?></h2>
     <hr>
 
-    <div class="grid-x align-center margin-top-2">
-      <div class="small-10 medium-6">
+    <div class="grid-x align-center">
+      <article class="small-10 medium-8 large-6">
+        <h1 class="margin-top-2" style="line-height: 1"><?= $post->title; ?></h1>
+        <p class="lead font-italic margin-bottom-2">
+          <time>
+            <?php
+            $date = new DateTime($post->created_at);
+            echo $date->format('M d, Y');
+            ?>
+          </time>
+          by John Doe
+        </p>
         <?php if($post->cover) : ?>
-          <img class="margin-bottom-1" src="/img/<?= $post->cover; ?>" alt="">
+          <picture>
+            <source media="(min-width: 768px)"
+                    srcset="/img/<?= $post->cover; ?> 1x,
+                            /img/lg-<?= $post->cover; ?> 2x">
+            <img src="/img/sm-<?= $post->cover; ?>"
+                 srcset="/img/<?= $post->cover; ?> 2x"
+                 alt="" style="width: 100%">
+          </picture>
         <?php endif; ?>
-        <div><?= $post->content; ?></div>
-      </div>
+        <div class="margin-top-2"><?= $post->content; ?></div>
+      </article>
     </div>
-
   </div>
   <?php require __DIR__.'/../partials/scripts.php'; ?>
   <script>
