212d5ab086a6589ce0c3ed928b0202a3871d03b3
Sébastien Pereda <sebastien.pereda@gmail.com>
2018-11-29 09:25:18 +0100

    Changed: Présentation de la page d’index des articles (media objects)

--- app/views/posts/index.view.php
+++ app/views/posts/index.view.php
@@ -3,10 +3,32 @@
   <div class="grid-x align-center">
     <div class="small-10 medium-6">
       <?php if (count($posts) > 0) : ?>
-        <?php foreach (array_reverse($posts) as $post) : ?>
-          <a href="/posts/<?= $post->id; ?>">
-            <p class="callout"><?= $post->title; ?></p>
+        <?php foreach ($posts as $post) : ?>
+          <a href="/posts/<?= $post->id ?>">
+            <div class="media-object">
+              <?php if ($post->cover) : ?>
+                <div class="media-object-section">
+                  <div class="thumbnail">
+                    <img src="/img/sm-<?= $post->cover ?>" width="175">
+                  </div>
+                </div>
+              <?php endif; ?>
+              <div class="media-object-section">
+                <h4 class="margin-bottom-0"><?= $post->title ?></h4>
+                <p class="font-italic">
+                  <time>
+                    <?php
+                      $date = new DateTime($post->created_at);
+                      echo $date->format('M d, Y');
+                    ?>
+                  </time>
+                  by John Doe
+                </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet at atque deserunt.</p>
+              </div>
+            </div>
           </a>
+          <hr>
         <?php endforeach; ?>
       <?php else : ?>
         <p class="lead text-center">There are no articles to display.</p>
