9df7df0c263fb36cf121c77897881e983e613ce9
Sébastien Pereda <sebastien.pereda@gmail.com>
2019-01-29 20:24:40 +0100

    Changed: amélioration script carousel (changement du texte alternatif)

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,20 +1,21 @@
 let images = [
-    "img/cat2.jpeg",
-    "img/cat3.jpeg",
-    "img/cat4.jpeg",
-    "img/cat5.jpeg",
-    "img/cat6.jpeg",
-    "img/cat7.jpeg",
-    "img/cat8.jpeg",
-    "img/cat9.jpeg",
-    "img/cat1.jpeg",
+    ["img/cat2.jpeg", 'Lion qui marche sur une route'],
+    ["img/cat3.jpeg", 'Chat couché sur le côté'],
+    ["img/cat4.jpeg", 'Léopard'],
+    ["img/cat5.jpeg", 'Chat qui aime les caresses dans le cou'],
+    ["img/cat6.jpeg", 'Léopard'],
+    ["img/cat7.jpeg", 'Chat qui sort la tête du couvre-lit'],
+    ["img/cat8.jpeg", 'Tigre dans l’eau'],
+    ["img/cat9.jpeg", 'Chat qui ferme les yeux'],
+    ["img/cat1.jpeg", 'Chat sous la couette'],
 ];
 
 let img = document.querySelector('.carousel');
 let i = 0;
 
 function changeImage() {
-    img.setAttribute('src', images[i]);
+    img.setAttribute('src', images[i][0]);
+    img.setAttribute('alt', images[i][1]);
     i++;
     if ( i >= images.length ) {
         i = 0;
