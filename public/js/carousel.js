a8242f2e37dd81bbe866441b98cddcc48e0b3dca
Sébastien Pereda <sebastien.pereda@gmail.com>
2019-02-03 19:12:21 +0100

    Refact: réorganisation des images

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,13 +1,13 @@
 let images = [
-    ["img/cat2.jpeg", 'Lion qui marche sur une route'],
-    ["img/cat3.jpeg", 'Chat couché sur le côté'],
-    ["img/cat4.jpeg", 'Léopard'],
-    ["img/cat5.jpeg", 'Chat qui aime les caresses dans le cou'],
-    ["img/cat6.jpeg", 'Léopard'],
-    ["img/cat7.jpeg", 'Chat qui sort la tête du couvre-lit'],
-    ["img/cat8.jpeg", 'Tigre dans l’eau'],
-    ["img/cat9.jpeg", 'Chat qui ferme les yeux'],
-    ["img/cat1.jpeg", 'Chat sous la couette'],
+    ["img/carouel/cat2.jpeg", 'Lion qui marche sur une route'],
+    ["img/carouel/cat3.jpeg", 'Chat couché sur le côté'],
+    ["img/carouel/cat4.jpeg", 'Léopard'],
+    ["img/carouel/cat5.jpeg", 'Chat qui aime les caresses dans le cou'],
+    ["img/carouel/cat6.jpeg", 'Léopard'],
+    ["img/carouel/cat7.jpeg", 'Chat qui sort la tête du couvre-lit'],
+    ["img/carouel/cat8.jpeg", 'Tigre dans l’eau'],
+    ["img/carouel/cat9.jpeg", 'Chat qui ferme les yeux'],
+    ["img/carouel/cat1.jpeg", 'Chat sous la couette'],
 ];
 
 let img = document.querySelector('.carousel');
