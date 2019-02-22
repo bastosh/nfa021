a3821a28afba738d4aa5493803740a8f7ff9956a
Sébastien Pereda <sebastien.pereda@gmail.com>
2019-02-22 22:15:14 +0100

    Fixed: traduction bouton de retour

--- app/views/admin/feature.view.php
+++ app/views/admin/feature.view.php
@@ -3,7 +3,7 @@
   <!-- CONTENT -->
   <div class="app-dashboard-body-content off-canvas-content" data-off-canvas-content>
 
-    <a href="/admin-features"><i class="fas fa-long-arrow-alt-left"></i> Back</a>
+    <a href="/admin-features"><i class="fas fa-long-arrow-alt-left"></i> Retour aux spécialités</a>
 
     <h2 class="text-center"><?= $feature->title; ?></h2>
 
